ccf6442d62365b8a7b0130ce891086bc145c17fc
Erdong Feng <edfeng@thoughtworks.com>
2015-04-03 09:22:02 +0800

    more solutions for problem b(use string slice).

--- chapter2/three_questions/raw_solutions/question_b.py
+++ chapter2/three_questions/raw_solutions/question_b.py
@@ -2,7 +2,19 @@ from collections import deque
 
 __author__ = 'edfeng'
 
+
+def solution1(s, t):
+    d = deque(s)
+    d.rotate(t)
+    return ''.join(d)
+
+
+def solution2(s, t):
+    return s[-t:] + s[:-t]
+
+
 if __name__ == '__main__':
-    d = deque("abcdefgh")
-    d.rotate(-3)
-    print ''.join(d)
+    assert solution1("abcdefgh", -3) == "defghabc"
+    assert solution1("abcdefgh", 3) == "fghabcde"
+    assert solution2("abcdefgh", -3) == "defghabc"
+    assert solution2("abcdefgh", 3) == "fghabcde"
